6b2a526eb5af29de44afbbcfcad129136c4c3301
Lee Henson <lee.m.henson@gmail.com>
2019-02-27 13:23:15 +0000

    mismatched versions

--- company.ts
+++ company.ts
@@ -1,6 +1,7 @@
 import { makeEither } from "sp/user";
 import { either } from "fp-ts/lib/Either";
 import { option } from "fp-ts/lib/Option";
+import { HKT2 } from "fp-ts/lib/HKTv14_1";
 import { Thing } from "sp/thing";
 
 export const doThing = () =>
--- subproject/user.ts
+++ subproject/user.ts
@@ -1,4 +1,5 @@
 import { either } from "fp-ts/lib/Either";
+import { URI2HKT2 } from "fp-ts/lib/HKTv14_1";
 import { Thing } from "./thing";
 
 export const makeEither = () => either.of<string, boolean>(true);
